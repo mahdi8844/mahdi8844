5d1996604dd23976aa95c8b73980efc17c806484
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-05-20 23:18:41 +0800

    Delete movieha.js

--- movieha.js
+++
@@ -1,6 +0,0 @@
-var dataUrl = [
-  
-  'https://www.achievablecpmrevenue.com/us5t0cbw?key=1c4590512e767b5008342ac53352ba10'
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
