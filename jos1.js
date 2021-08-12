8df2a7c54f0558ea3d1344f2032b564a2f113fd0
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-08-12 09:03:42 +0800

    Update jos1.js

--- jos1.js
+++ jos1.js
@@ -2,7 +2,10 @@ var dataUrl = [
   
   'https://adorableold.com/us5t0cbw?key=1c4590512e767b5008342ac53352ba10',
   'https://adorableold.com/nf3rpr9q98?key=3f4cd0c002b2da2412ce482c8620ad89',
-  'https://adorableold.com/wyks8zsng?key=174a4a965d724aac2f3d8c535fbda8b8'
+  'https://adorableold.com/wyks8zsng?key=174a4a965d724aac2f3d8c535fbda8b8',
+  'https://www.highperformancecpm.com/v4f3yczx?key=7fa2b29086b9322b0a2dfdbd36b9f724',
+  'https://www.highperformancecpm.com/g06xg3eg5?key=26621851ec3ae7e1101746824d09c269'
+  
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
