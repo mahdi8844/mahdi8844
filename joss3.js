36728ab96d3b0b93b5b41edc54cfb1b5f1432611
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-08-28 11:17:15 +0800

    Update joss3.js

--- joss3.js
+++ joss3.js
@@ -1,7 +1,9 @@
 var dataUrl = [
   
   'https://www.highperformancecpm.com/dc4j91c7v?key=9b2445d7fd2d084d5beb8e3db8d52fd5',
-  'https://www.highperformancecpm.com/uyh47i57?key=521dbcfa9c33f09f9006287ca4487e30'
+  'https://www.highperformancecpm.com/uyh47i57?key=521dbcfa9c33f09f9006287ca4487e30',
+  'https://www.highperformancecpm.com/jsn2p5i6nv?key=50185e6f543cccf5c24c6890a58a8402',
+  'https://www.highperformancecpm.com/piqsw0uayr?key=4b8aed12b0792a0c5bbb84fa924338ed'
   
   
 ];
