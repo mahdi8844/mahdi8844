bbbdcd852c2259d0d8862db5655be9b4b0cd8499
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-09-17 14:56:10 +0800

    Create sementara.js

---
+++ sementara.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+  
+  'https://www.effectivecpmgate.com/jcfn4nczbt?key=b33e4648c1fe6d6bba6f89c9470e38a0'
+  
+ 
+  
+  
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
+
