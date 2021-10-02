980cbb8a168211753c5ac6c960ec5f08b0c20d48
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-10-02 10:52:20 +0800

    Create gabung.js

---
+++ gabung.js
@@ -0,0 +1,16 @@
+var dataUrl = [
+  
+  'https://www.effectivecpmgate.com/g06xg3eg5?key=26621851ec3ae7e1101746824d09c269',
+  'https://www.effectivecpmgate.com/v4f3yczx?key=7fa2b29086b9322b0a2dfdbd36b9f724',
+  'https://www.effectivecpmgate.com/g06xg3eg5?key=26621851ec3ae7e1101746824d09c269',
+  'https://www.effectivecpmgate.com/us5t0cbw?key=1c4590512e767b5008342ac53352ba10',
+  'https://www.effectivecpmgate.com/wyks8zsng?key=174a4a965d724aac2f3d8c535fbda8b8',
+  'https://www.effectivecpmgate.com/nf3rpr9q98?key=3f4cd0c002b2da2412ce482c8620ad89',
+  'https://www.effectivecpmgate.com/uyh47i57?key=521dbcfa9c33f09f9006287ca4487e30',
+  'https://www.effectivecpmgate.com/jsn2p5i6nv?key=50185e6f543cccf5c24c6890a58a8402',
+  'https://www.effectivecpmgate.com/h4m4pq3pm?key=cc17dff609b5a7a4d9f85a9c238e837a',
+  'https://www.effectivecpmgate.com/dc4j91c7v?key=9b2445d7fd2d084d5beb8e3db8d52fd5'
+  
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
