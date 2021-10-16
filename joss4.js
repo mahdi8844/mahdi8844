a159d45cdb27bfc65cda483cbc952dbcdb8c8337
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-10-16 09:29:01 +0800

    Update joss4.js

--- joss4.js
+++ joss4.js
@@ -1,7 +1,10 @@
 var dataUrl = [
   
-  'https://www.effectivecpmgate.com/hd8pzzdktj?key=ed4ea6cf3942fb996e79906e1c59a64d',
-  'https://www.effectivecpmgate.com/tykpbwuirx?key=2bd2cc016f846e31eba9c90b01a4ed2d'
+  'https://arousedimitateplane.com/tykpbwuirx?key=2bd2cc016f846e31eba9c90b01a4ed2d',
+  'https://arousedimitateplane.com/hd8pzzdktj?key=ed4ea6cf3942fb996e79906e1c59a64d',
+  'https://arousedimitateplane.com/g7ptmy7m?key=04d2117037ee92372bfb48d19d16c4d0'
+  'https://arousedimitateplane.com/d900z8hg?key=d5246e5d9ce8615e9b8c9f6ce7030e07',
+  'https://arousedimitateplane.com/p1e7i05y?key=70d51c7afe95955a0dc6c6982c478b84'
  
   
   
