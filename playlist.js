1cba122a7eb6a67e918d02b8a05fa3cda508ae5a
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-10-24 19:32:18 +0800

    Create playlist.js

---
+++ playlist.js
@@ -0,0 +1,13 @@
+
+var dataUrl = [
+  
+  'https://www.trustedgatetocontent.com/uyh47i57?key=521dbcfa9c33f09f9006287ca4487e30',
+  'https://www.trustedgatetocontent.com/dc4j91c7v?key=9b2445d7fd2d084d5beb8e3db8d52fd5',
+  'https://www.trustedgatetocontent.com/h4m4pq3pm?key=cc17dff609b5a7a4d9f85a9c238e837a',
+  'https://www.trustedgatetocontent.com/piqsw0uayr?key=4b8aed12b0792a0c5bbb84fa924338ed',
+  'https://www.trustedgatetocontent.com/jsn2p5i6nv?key=50185e6f543cccf5c24c6890a58a8402'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
+ad
