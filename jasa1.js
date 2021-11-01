ebdcf6687bfb377b4c023257a36a719816bd2194
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-11-02 06:36:21 +0800

    Update jasa1.js

--- jasa1.js
+++ jasa1.js
@@ -3,8 +3,8 @@ var dataUrl = [
   
   'https://accusationcollegeload.com/hpf2wcm4?key=e60866539394fcb1d524ec2677eb5173',
   'https://accusationcollegeload.com/mvffv3wtan?key=9fd2018daddabecf48cef6b26570d94c',
-  'https://accusationcollegeload.com/n5q1d7fqv6?key=eee8425f3fe6abdd275d14e94b4c920b',
-  'https://www.trustedgatetocontent.com/hx3g9hvzd?key=8ffd8310adecd93c5ec27d2ed8e75016'
+  'https://accusationcollegeload.com/n5q1d7fqv6?key=eee8425f3fe6abdd275d14e94b4c920b'
+ 
 
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
