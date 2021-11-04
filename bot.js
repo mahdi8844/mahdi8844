7487069f1ec64292207d495d6ca79beaea323574
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-11-04 12:13:13 +0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,10 @@
+var dataUrl = [
+  
+  'https://www.trustedgatetocontent.com/w11f6fh2ua?key=aeffd39951a7f32c2bcf38fc8a4ae4d8',
+  'https://www.trustedgatetocontent.com/sjkfhz8m3?key=db7f349f9a05ee1b03fb0c88a9657693',
+  'https://www.trustedgatetocontent.com/z1wn7uchfh?key=8e7e9cee0a6e0c3072355923040687d0'
+
+  
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
