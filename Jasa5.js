97d3958d8fdc107e212c68b3492caf8e2514d928
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-11-09 11:38:13 +0800

    Create Jasa5.js

---
+++ Jasa5.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+  
+  'https://www.trustedgatetocontent.com/q3s7t7nfn0?key=bf941e54ccb924f6c8d410112a447906',
+  'https://www.trustedgatetocontent.com/uc8qz7uwz?key=9662cc459847aacc3fd3cd1d45d1c923',
+  'https://www.trustedgatetocontent.com/pigxa8es8?key=bd33381f7f9377cbe802574931452719' 
+  
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
