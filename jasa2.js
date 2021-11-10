7ba5c683a1b3b0a56068604f820af5fb353f3510
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-11-10 17:48:57 +0800

    Update jasa2.js

--- jasa2.js
+++ jasa2.js
@@ -3,7 +3,8 @@ var dataUrl = [
   
   'https://inadmissibleinsensitive.com/j29g9ietq?key=e24eba510c80b3ba9de9d465a7af2b5a',
   'https://inadmissibleinsensitive.com/gzfadn5d?key=0e05d5a31ff3702d059c7bc6c4517196',
-  'https://inadmissibleinsensitive.com/sqzweewk?key=d9f4e459965fdc155a14210a559c4290'
+  'https://inadmissibleinsensitive.com/sqzweewk?key=d9f4e459965fdc155a14210a559c4290',
+  'https://www.trustedgatetocontent.com/w11f6fh2ua?key=aeffd39951a7f32c2bcf38fc8a4ae4d8'
 
 
 ];
