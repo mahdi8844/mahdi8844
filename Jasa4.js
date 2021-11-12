dd28ce1ebe180d801b6b13bcf6e89efabce1e424
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-11-12 16:54:34 +0800

    Update Jasa4.js

--- Jasa4.js
+++ Jasa4.js
@@ -3,7 +3,7 @@ var dataUrl = [
   'https://divorceseed.com/pgnh3n5zwr?key=23286cf86550718e306100e690faba5a',
   'https://divorceseed.com/pgnh3n5zwr?key=23286cf86550718e306100e690faba5a',
   'https://divorceseed.com/u43t9mn7?key=c5b7aec72870c2b3a03b76d580d83e5d',
-  'https://www.trustedgatetocontent.com/w11f6fh2ua?key=aeffd39951a7f32c2bcf38fc8a4ae4d8'
+  'https://www.trustedgatetocontent.com/sjkfhz8m3?key=db7f349f9a05ee1b03fb0c88a9657693'
   
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
