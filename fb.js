bf30b70d00c2280e8899f838496219016d2a34c6
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-11-12 16:50:35 +0800

    Update fb.js

--- fb.js
+++ fb.js
@@ -3,7 +3,7 @@ var dataUrl = [
   
   'https://infesthazardous.com/q38569tt?key=c44d38ca5e7be1454d233de6c29c3adc',
   'https://infesthazardous.com/gp0h8aj0sy?key=c7a74a7bcb083f9c36c764b44c4830f7',
-  'https://www.trustedgatetocontent.com/hx3g9hvzd?key=8ffd8310adecd93c5ec27d2ed8e75016'
+  'https://www.trustedgatetocontent.com/sjkfhz8m3?key=db7f349f9a05ee1b03fb0c88a9657693'
 
 
 ];
