ec99e493847b98915cc248ccad348eb18265ba5d
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-11-13 21:59:26 +0800

    Update gabung1.js

--- gabung1.js
+++ gabung1.js
@@ -1,15 +1,15 @@
 var dataUrl = [
   
-  'https://www.trustedgatetocontent.com/g06xg3eg5?key=26621851ec3ae7e1101746824d09c269',
-  'https://www.trustedgatetocontent.com/v4f3yczx?key=7fa2b29086b9322b0a2dfdbd36b9f724',
-  'https://www.trustedgatetocontent.com/jcfn4nczbt?key=b33e4648c1fe6d6bba6f89c9470e38a0',
-  'https://www.trustedgatetocontent.com/us5t0cbw?key=1c4590512e767b5008342ac53352ba10',
-  'https://www.trustedgatetocontent.com/wyks8zsng?key=174a4a965d724aac2f3d8c535fbda8b8',
-  'https://www.trustedgatetocontent.com/nf3rpr9q98?key=3f4cd0c002b2da2412ce482c8620ad89',
-  'https://www.trustedgatetocontent.com/uyh47i57?key=521dbcfa9c33f09f9006287ca4487e30',
-  'https://www.trustedgatetocontent.com/dc4j91c7v?key=9b2445d7fd2d084d5beb8e3db8d52fd5',
-  'https://www.trustedgatetocontent.com/piqsw0uayr?key=4b8aed12b0792a0c5bbb84fa924338ed',
-  'https://www.trustedgatetocontent.com/h4m4pq3pm?key=cc17dff609b5a7a4d9f85a9c238e837a'
+  'https://www.effectivegatetocontent.com/jcfn4nczbt?key=b33e4648c1fe6d6bba6f89c9470e38a0',
+  'https://www.effectivegatetocontent.com/v4f3yczx?key=7fa2b29086b9322b0a2dfdbd36b9f724',
+  'https://www.effectivegatetocontent.com/g06xg3eg5?key=26621851ec3ae7e1101746824d09c269',
+  'https://www.effectivegatetocontent.com/us5t0cbw?key=1c4590512e767b5008342ac53352ba10',
+  'https://www.effectivegatetocontent.com/wyks8zsng?key=174a4a965d724aac2f3d8c535fbda8b8',
+  'https://www.effectivegatetocontent.com/nf3rpr9q98?key=3f4cd0c002b2da2412ce482c8620ad89',
+  'https://www.effectivegatetocontent.com/dc4j91c7v?key=9b2445d7fd2d084d5beb8e3db8d52fd5',
+  'https://www.effectivegatetocontent.com/uyh47i57?key=521dbcfa9c33f09f9006287ca4487e30',
+  'https://www.effectivegatetocontent.com/jsn2p5i6nv?key=50185e6f543cccf5c24c6890a58a8402',
+  'https://www.effectivegatetocontent.com/piqsw0uayr?key=4b8aed12b0792a0c5bbb84fa924338ed'
   
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
