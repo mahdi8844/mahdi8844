f14ccf1e161796ae76326966283e26af329f30e7
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-11-13 21:12:17 +0800

    Update Jasa7.js

--- Jasa7.js
+++ Jasa7.js
@@ -2,8 +2,8 @@ var dataUrl = [
   
   'https://ashamedbirchpoorly.com/cmykisskd?key=e68cb13ef15b9a23209e22bb495cfedb',
   'https://ashamedbirchpoorly.com/veifjzetx7?key=5155c968cdaa17dce8bfd794d234498b',
-  'https://ashamedbirchpoorly.com/q3uqxpib?key=fa141f19f87d78b1234fb48c1b001836'
-  
+  'https://ashamedbirchpoorly.com/q3uqxpib?key=fa141f19f87d78b1234fb48c1b001836',
+  'https://www.effectivegatetocontent.com/w11f6fh2ua?key=aeffd39951a7f32c2bcf38fc8a4ae4d8'
  
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
