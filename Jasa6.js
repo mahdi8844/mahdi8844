6aca9737136a8ecc6d35aefe043da8705f2e1cf3
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-11-13 10:01:09 +0800

    Create Jasa6.js

---
+++ Jasa6.js
@@ -0,0 +1,10 @@
+var dataUrl = [
+  
+  'https://levityprogramming.com/ef8f06scn?key=55bb0f8288c4367c44d86fdaf2e8fdaf',
+  'https://levityprogramming.com/zrt5t19z?key=c8e6842254d85158ff92524863eb66de',
+  'https://levityprogramming.com/bax8p0z0?key=4dba45e083911eeda83804eb0a6fb720'
+  
+  
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
