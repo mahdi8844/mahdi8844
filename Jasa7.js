4175c689e12c6c70c3041426d93ce598810b290c
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-11-13 10:08:29 +0800

    Create Jasa7.js

---
+++ Jasa7.js
@@ -0,0 +1,10 @@
+var dataUrl = [
+  
+  'https://ashamedbirchpoorly.com/cmykisskd?key=e68cb13ef15b9a23209e22bb495cfedb',
+  'https://ashamedbirchpoorly.com/veifjzetx7?key=5155c968cdaa17dce8bfd794d234498b',
+  'https://ashamedbirchpoorly.com/q3uqxpib?key=fa141f19f87d78b1234fb48c1b001836'
+  
+ 
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
