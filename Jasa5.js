f37316b6c14fece02918ce5bd37fec96e7491b63
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-11-13 21:17:21 +0800

    Update Jasa5.js

--- Jasa5.js
+++ Jasa5.js
@@ -3,7 +3,7 @@ var dataUrl = [
   'https://www.trustedgatetocontent.com/q3s7t7nfn0?key=bf941e54ccb924f6c8d410112a447906',
   'https://www.trustedgatetocontent.com/uc8qz7uwz?key=9662cc459847aacc3fd3cd1d45d1c923',
   'https://www.trustedgatetocontent.com/pigxa8es8?key=bd33381f7f9377cbe802574931452719',
-  'https://www.trustedgatetocontent.com/sjkfhz8m3?key=db7f349f9a05ee1b03fb0c88a9657693'
+  'https://www.effectivegatetocontent.com/sjkfhz8m3?key=db7f349f9a05ee1b03fb0c88a9657693'
   
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
