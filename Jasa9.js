d9cec3865f60e001c313e08c13e253029eaaf570
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-11-13 21:13:41 +0800

    Update Jasa9.js

--- Jasa9.js
+++ Jasa9.js
@@ -2,8 +2,8 @@ var dataUrl = [
   
   'https://resemblanceilluminatedcigarettes.com/pre0etqm?key=38ac1a47adb33c2188e38c202cbebc13',
   'https://resemblanceilluminatedcigarettes.com/y8dxgsmqrs?key=5828a31e307f8661b5e8312efe659b3d',
-  'https://resemblanceilluminatedcigarettes.com/qir3gynkpk?key=66610dd11038890adaff5b9b496f3cff'
-  
+  'https://resemblanceilluminatedcigarettes.com/qir3gynkpk?key=66610dd11038890adaff5b9b496f3cff',
+  'https://www.effectivegatetocontent.com/w11f6fh2ua?key=aeffd39951a7f32c2bcf38fc8a4ae4d8'
  
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
