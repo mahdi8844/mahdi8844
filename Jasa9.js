46d045a1817ae01493e379a3854ed99705c6d6e3
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-11-13 10:54:20 +0800

    Create Jasa9.js

---
+++ Jasa9.js
@@ -0,0 +1,10 @@
+var dataUrl = [
+  
+  'https://resemblanceilluminatedcigarettes.com/pre0etqm?key=38ac1a47adb33c2188e38c202cbebc13',
+  'https://resemblanceilluminatedcigarettes.com/y8dxgsmqrs?key=5828a31e307f8661b5e8312efe659b3d',
+  'https://resemblanceilluminatedcigarettes.com/qir3gynkpk?key=66610dd11038890adaff5b9b496f3cff'
+  
+ 
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
