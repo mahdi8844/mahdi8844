e1475a180232f6038dee6852908e243bae329e8b
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-11-13 21:14:15 +0800

    Update Jasa10.js

--- Jasa10.js
+++ Jasa10.js
@@ -2,8 +2,8 @@ var dataUrl = [
   
   'https://heartilyfootindebted.com/fw26g2qiw?key=7cacc0eadcff65a366c9fc57038d1901',
   'https://heartilyfootindebted.com/bnc7tqvhc?key=1e18de60d6bd451e2bb7e2a3c96d9f44',
-  'https://heartilyfootindebted.com/yptd1qe2s8?key=cc90da53dbdec1cfe5586bcb040c540b'
-  
+  'https://heartilyfootindebted.com/yptd1qe2s8?key=cc90da53dbdec1cfe5586bcb040c540b', 
+  'https://www.effectivegatetocontent.com/w11f6fh2ua?key=aeffd39951a7f32c2bcf38fc8a4ae4d8'
  
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
