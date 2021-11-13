3897f31b9ad0152d0bafbddc6d7b9c6576290f2f
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-11-13 10:20:33 +0800

    Create Jasa8.js

---
+++ Jasa8.js
@@ -0,0 +1,10 @@
+var dataUrl = [
+  
+  'https://conjunctionrepresentativepowerless.com/tg037viyk?key=5e7b486db7e0c9e4a075f8d486c3cf87',
+  'https://conjunctionrepresentativepowerless.com/pt5d5bgz2?key=891c3fb1903e9a7a8f7d9e4cd0ced3e1',
+  'https://conjunctionrepresentativepowerless.com/fqc6bhijns?key=0dff7f0f0e2e7216ecd85259f864021b'
+  
+ 
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
