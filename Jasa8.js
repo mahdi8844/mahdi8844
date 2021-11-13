88697d157223a294ebd13e84fc7e4ae8dec0c755
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-11-13 21:13:07 +0800

    Update Jasa8.js

--- Jasa8.js
+++ Jasa8.js
@@ -2,8 +2,8 @@ var dataUrl = [
   
   'https://conjunctionrepresentativepowerless.com/tg037viyk?key=5e7b486db7e0c9e4a075f8d486c3cf87',
   'https://conjunctionrepresentativepowerless.com/pt5d5bgz2?key=891c3fb1903e9a7a8f7d9e4cd0ced3e1',
-  'https://conjunctionrepresentativepowerless.com/fqc6bhijns?key=0dff7f0f0e2e7216ecd85259f864021b'
-  
+  'https://conjunctionrepresentativepowerless.com/fqc6bhijns?key=0dff7f0f0e2e7216ecd85259f864021b',
+  'https://www.effectivegatetocontent.com/w11f6fh2ua?key=aeffd39951a7f32c2bcf38fc8a4ae4d8'
  
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
