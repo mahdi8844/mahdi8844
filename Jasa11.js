54607bd2628aee10980f71069a1170d68aae05f9
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-11-14 23:36:33 +0800

    Create Jasa11.js

---
+++ Jasa11.js
@@ -0,0 +1,10 @@
+var dataUrl = [
+  
+  'https://www.effectivegatetocontent.com/g07wb7ti?key=9610b0fac903371b7a460a721da454bd',
+  'https://www.effectivegatetocontent.com/cj1qd4cx9g?key=5192e1de6a737b942275f7b5d2414529',
+  'https://www.effectivegatetocontent.com/ndu6wcdvi?key=56a4f49332a54f16a1bdab1ad8914985' 
+  
+ 
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
