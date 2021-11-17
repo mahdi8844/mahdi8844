bb4564519b356270faf6af5f0740d709e8bb27fb
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-11-18 01:43:09 +0800

    Create Jasa15.js

---
+++ Jasa15.js
@@ -0,0 +1,10 @@
+var dataUrl = [
+  
+  'https://juvenilebind.com/fuumidgrcz?key=4f931b383534a988d0f8b38cf77baa8e',
+  'https://juvenilebind.com/nd6b6jgp7?key=765680f348f359a38ccefdbf8c9291ab',
+  'https://juvenilebind.com/hk2nur2a?key=2788a3580dc21c89a0ddb4f688f1fd47', 
+  'https://www.effectivegatetocontent.com/w11f6fh2ua?key=aeffd39951a7f32c2bcf38fc8a4ae4d8'
+ 
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
