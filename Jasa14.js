32e28b58d29fe43c54f6355cfe678de1b07ec340
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-11-18 01:18:03 +0800

    Create Jasa14.js

---
+++ Jasa14.js
@@ -0,0 +1,10 @@
+var dataUrl = [
+  
+  'https://dippingearlier.com/w34y4tbie?key=047134d40c63136bbb84c81c6849cd3b',
+  'https://dippingearlier.com/nmjn4enuu6?key=05a5d04600427802afc5f3efb797d246',
+  'https://dippingearlier.com/wshjicfk4c?key=23dcf4e43b3aec8638f0447a9e4d1203', 
+  'https://www.effectivegatetocontent.com/w11f6fh2ua?key=aeffd39951a7f32c2bcf38fc8a4ae4d8'
+ 
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
