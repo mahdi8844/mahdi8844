79719e5c5ecb2c0ae962fe21361b70095ae5199e
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2021-12-15 16:32:28 +0800

    Update joss5c.js

--- joss5c.js
+++ joss5c.js
@@ -3,7 +3,8 @@ var dataUrl = [
   
   'https://arousedimitateplane.com/tykpbwuirx?key=2bd2cc016f846e31eba9c90b01a4ed2d',
   'https://arousedimitateplane.com/hd8pzzdktj?key=ed4ea6cf3942fb996e79906e1c59a64d',
-  'https://arousedimitateplane.com/p1e7i05y?key=70d51c7afe95955a0dc6c6982c478b84'
+  'https://arousedimitateplane.com/p1e7i05y?key=70d51c7afe95955a0dc6c6982c478b84',
+  'https://www.effectivegatetocontent.com/bk6xziud?key=24a93215ae3fd40b02e4035bb344aa7b'
   
   
 
