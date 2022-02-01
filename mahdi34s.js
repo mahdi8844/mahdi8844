ecf501f08e6e62a32209757f704a8d387611e061
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-02-01 09:27:46 +0800

    Update and rename mahdi34.js to mahdi34s.js

--- mahdi34s.js
+++ mahdi34s.js
@@ -2,8 +2,6 @@ var dataUrl = [
   
   'https://arousedimitateplane.com/tykpbwuirx?key=2bd2cc016f846e31eba9c90b01a4ed2d',
   'https://annoynoveltyeel.com/bk6xziud?key=24a93215ae3fd40b02e4035bb344aa7b',
-  'https://arousedimitateplane.com/hd8pzzdktj?key=ed4ea6cf3942fb996e79906e1c59a64d',
-  'https://annoynoveltyeel.com/wbbzz1u91r?key=d0ccda3580917dc6f6247f5013028171',
   'https://arousedimitateplane.com/p1e7i05y?key=70d51c7afe95955a0dc6c6982c478b84',
   'https://annoynoveltyeel.com/s18bncwz?key=d185a86de0729a4f4e0943ca13d6d8ee'
   
