993665c9f869d6dfd2f6d9bb590f46439543a3b9
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-02-01 09:44:25 +0800

    Update mahdi34s.js

--- mahdi34s.js
+++ mahdi34s.js
@@ -1,9 +1,9 @@
 var dataUrl = [
   
   'https://arousedimitateplane.com/tykpbwuirx?key=2bd2cc016f846e31eba9c90b01a4ed2d',
-  'https://annoynoveltyeel.com/bk6xziud?key=24a93215ae3fd40b02e4035bb344aa7b',
-  'https://arousedimitateplane.com/p1e7i05y?key=70d51c7afe95955a0dc6c6982c478b84',
-  'https://annoynoveltyeel.com/s18bncwz?key=d185a86de0729a4f4e0943ca13d6d8ee'
+  'https://annoynoveltyeel.com/bk6xziud?key=24a93215ae3fd40b02e4035bb344aa7b'
+  
+  
   
   
   
