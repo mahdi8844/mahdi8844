5922fe2e976461d4ca8eee55b73a6d53ff5d73ae
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-06-15 15:53:40 +0800

    Update gabung.js

--- gabung.js
+++ gabung.js
@@ -1,8 +1,10 @@
 var dataUrl = [
   
 
-  'https://www.profitablecpmgate.com/mr8kenhu8r?key=db7b9e6af3a247db6cc5ea2058829856',
-  'https://www.profitablecpmgate.com/fsxjit4bfr?key=33fde0dc943053e2bceba9e6a4915afa'
+  'https://www.profitablecpmgate.com/rdth62kn?key=fe2d611271da8c04daf4c48a27945305',
+  'https://www.profitablecpmgate.com/gwpm4n4406?key=22d42f69a9a323884fd28cc55958c1eb', 
+  'https://www.profitablecpmgate.com/us5t0cbw?key=1c4590512e767b5008342ac53352ba10', 
+  'https://www.profitablecpmgate.com/nf3rpr9q98?key=3f4cd0c002b2da2412ce482c8620ad89'
   
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
