19f8b40ff3c69838b1b2a5887e32e3f40a419bb7
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-06-15 16:00:20 +0800

    Update mahdi34s.js

--- mahdi34s.js
+++ mahdi34s.js
@@ -1,7 +1,7 @@
 var dataUrl = [
   
-  'https://arousedimitateplane.com/qqw44zz8ep?key=5b37e5e645b8b4a817e3c70f3fcd1b09',
-  'https://www.profitablecpmgate.com/q3dt60cz?key=ad87793ad0a063f2f739f87672bea5fe'
+  'https://arousedimitateplane.com/eq27arfej?key=67b1fb0cda078a4ea41d8558e49e07d4',
+  'https://www.profitablecpmgate.com/s08bg6xt?key=282003c5c880a35c2edd8003e1d9c33b'
   
   
   
