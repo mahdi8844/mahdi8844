3b9d0f4b7963aebcdbc3562fd122f800ca547816
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-06-15 13:27:33 +0800

    Update gabung.js

--- gabung.js
+++ gabung.js
@@ -1,15 +1,8 @@
 var dataUrl = [
   
-  'https://www.profitablecpmgate.com/g06xg3eg5?key=26621851ec3ae7e1101746824d09c269',
-  'https://www.profitablecpmgate.com/v4f3yczx?key=7fa2b29086b9322b0a2dfdbd36b9f724',
-  'https://www.profitablecpmgate.com/jcfn4nczbt?key=b33e4648c1fe6d6bba6f89c9470e38a0',
-  'https://www.profitablecpmgate.com/us5t0cbw?key=1c4590512e767b5008342ac53352ba10',
-  'https://www.profitablecpmgate.com/wyks8zsng?key=174a4a965d724aac2f3d8c535fbda8b8',
-  'https://www.profitablecpmgate.com/nf3rpr9q98?key=3f4cd0c002b2da2412ce482c8620ad89',
-  'https://www.profitablecpmgate.com/dc4j91c7v?key=9b2445d7fd2d084d5beb8e3db8d52fd5',
-  'https://www.profitablecpmgate.com/uyh47i57?key=521dbcfa9c33f09f9006287ca4487e30',
-  'https://www.profitablecpmgate.com/jsn2p5i6nv?key=50185e6f543cccf5c24c6890a58a8402',
-  'https://www.profitablecpmgate.com/piqsw0uayr?key=4b8aed12b0792a0c5bbb84fa924338ed'
+
+  'https://www.profitablecpmgate.com/mr8kenhu8r?key=db7b9e6af3a247db6cc5ea2058829856',
+  'https://www.profitablecpmgate.com/fsxjit4bfr?key=33fde0dc943053e2bceba9e6a4915afa'
   
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
