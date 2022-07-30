6c9f7104dac2b13a854c291fd231c0725720fb1b
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-07-30 14:05:04 +0800

    Create ansarinew2.js

---
+++ ansarinew2.js
@@ -0,0 +1,25 @@
+var dataUrl = [
+  
+  'https://www.highperformancegate.com/jpp55717e?key=8507cef1a397afc5fea1dc360c0886b3',
+  'https://www.highperformancegate.com/j1xw0x0rcx?key=1c9adcc6bca3296fac552e8c1e5b0a82',
+  'https://www.highperformancegate.com/efem9wbsk?key=b2380e7e2441eb8676f2bfad8134ee97',
+  'https://www.highperformancegate.com/i3c11jfyzq?key=1cd656c0804d50396e719a76a043e8e8',
+  'https://www.highperformancegate.com/up6wxiqm91?key=f665e3bb7e8a4bf4c6c8cb38f693c707',
+  'https://wednesdaynaked.com/bq60261s?key=4268643d1293b7a87dda445d6f988ece',
+  'https://wednesdaynaked.com/n6vefjuang?key=6d6c403700d7667dd3c9eb193df5c41d',
+  'https://fireworkswad.com/nx1jfdzr?key=8a9b83430981254c00c2804ddb2b7d92',
+  'https://fireworkswad.com/yevb1vpg2?key=0127f56241a83ccd37506a3d59a797fe',
+  'https://fireworkswad.com/h8iv88se1v?key=21893a1ff00c4483973d3efc2febd04c',
+  'https://fireworkswad.com/kdvahsb0wj?key=66f1e59b1b551fb24893ae895dc1d658',
+  'https://fireworkswad.com/rm5e0t7wd?key=d1c7d5a4bfef1f1a791c9f7c5cb38ec7'
+
+ 
+
+  
+  
+  
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
+
