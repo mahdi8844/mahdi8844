5b7cffb99d49cd3793211833779b48d6f069a479
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-08-14 19:11:47 +0800

    Update gabung.js

--- gabung.js
+++ gabung.js
@@ -1,19 +1,7 @@
 var dataUrl = [
   
-
-  'https://www.highperformancegate.com/rdth62kn?key=fe2d611271da8c04daf4c48a27945305',
-  'https://www.highperformancegate.com/dc4j91c7v?key=9b2445d7fd2d084d5beb8e3db8d52fd5',
-  'https://www.highperformancegate.com/uyh47i57?key=521dbcfa9c33f09f9006287ca4487e30',
-  'https://www.highperformancegate.com/mr8kenhu8r?key=db7b9e6af3a247db6cc5ea2058829856',
-  'https://www.highperformancegate.com/hx3g9hvzd?key=8ffd8310adecd93c5ec27d2ed8e75016',
-  'https://www.highperformancegate.com/gwpm4n4406?key=22d42f69a9a323884fd28cc55958c1eb', 
-  'https://www.highperformancegate.com/us5t0cbw?key=1c4590512e767b5008342ac53352ba10', 
-  'https://www.highperformancegate.com/v4f3yczx?key=7fa2b29086b9322b0a2dfdbd36b9f724',
-  'https://www.highperformancegate.com/fsxjit4bfr?key=33fde0dc943053e2bceba9e6a4915afa',
-  'https://www.highperformancegate.com/g06xg3eg5?key=26621851ec3ae7e1101746824d09c269',
-  'https://www.highperformancegate.com/jcfn4nczbt?key=b33e4648c1fe6d6bba6f89c9470e38a0',
-  'https://www.highperformancegate.com/nf3rpr9q98?key=3f4cd0c002b2da2412ce482c8620ad89'
-  
+'https://www.highperformancegate.com/hx3g9hvzd?key=8ffd8310adecd93c5ec27d2ed8e75016',
+'https://www.highperformancegate.com/fsxjit4bfr?key=33fde0dc943053e2bceba9e6a4915afa'
   
   
 ];
