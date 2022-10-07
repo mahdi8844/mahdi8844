f76a96d646ad6b54f434d0813d9933544e18fb56
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-10-07 09:38:49 +0800

    Update gabung.js

--- gabung.js
+++ gabung.js
@@ -1,9 +1,9 @@
 var dataUrl = [
   
-'https://www.highperformancegate.com/hx3g9hvzd?key=8ffd8310adecd93c5ec27d2ed8e75016',
-'https://www.highperformancegate.com/fsxjit4bfr?key=33fde0dc943053e2bceba9e6a4915afa',
-'https://arousedimitateplane.com/eq27arfej?key=67b1fb0cda078a4ea41d8558e49e07d4',
-'https://www.highperformancegate.com/w11f6fh2ua?key=aeffd39951a7f32c2bcf38fc8a4ae4d8'
+'https://www.profitablegatetocontent.com/hx3g9hvzd?key=8ffd8310adecd93c5ec27d2ed8e75016',
+'https://www.profitablegatetocontent.com/hx3g9hvzd?key=8ffd8310adecd93c5ec27d2ed8e75016',
+'https://highestfollowing.com/eq27arfej?key=67b1fb0cda078a4ea41d8558e49e07d4'
+
   
   
 ];
