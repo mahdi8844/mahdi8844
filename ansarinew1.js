788b1d36906341e29f05db3da7d48a81445b68c1
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-10-20 10:52:10 +0800

    Update ansarinew1.js

--- ansarinew1.js
+++ ansarinew1.js
@@ -1,20 +1,20 @@
 var dataUrl = [
   
-  'https://themeillogical.com/j1rxj172?key=adc36aa55f17d2c41223d390f5d7be1e',
-  'https://themeillogical.com/xyhh9ebt?key=70bf1cf20c9c44d1a9b9079b1159855b',
-  'https://themeillogical.com/fx19rsb805?key=9a451e61fa6b94f030eb813c38aa046c',
-  'https://themeillogical.com/d33wd6ank?key=115c23b469ea61aa266586a912ebeac6',
-  'https://themeillogical.com/xkd75qyzj?key=da41d9284ef2ccab8959f1b2e47c8fc7',
-  'https://grantedpigsunborn.com/feb7xbxrb?key=4e2587c12cf4b8990532d81ac580ce1f',
-  'https://grantedpigsunborn.com/b9z3pexh7c?key=241a8547e9c0e7911d728d3ad6aac6b4',
-  'https://grantedpigsunborn.com/jg8vjg3xwy?key=edd04c0b0bd41c0bd8494c2890390ad4',
-  'https://grantedpigsunborn.com/mmj2k7nrdt?key=a9adec45b7e8413e8085337ca17f76bf',
-  'https://grantedpigsunborn.com/eun6bc7z?key=7eb35efd04314c17bed2c3b7411852d9',
-  'https://amendablepredicamentvertical.com/ftevmc9ffu?key=88b2c5bdae56a3d0c808da01c98c12d7',
+  'https://www.profitablegatetocontent.com/fx19rsb805?key=9a451e61fa6b94f030eb813c38aa046c',
+  'https://www.profitablegatetocontent.com/j1rxj172?key=adc36aa55f17d2c41223d390f5d7be1e',
+  'https://www.profitablegatetocontent.com/xyhh9ebt?key=70bf1cf20c9c44d1a9b9079b1159855b',
+  'https://www.profitablegatetocontent.com/d33wd6ank?key=115c23b469ea61aa266586a912ebeac6',
+  'https://www.profitablegatetocontent.com/xkd75qyzj?key=da41d9284ef2ccab8959f1b2e47c8fc7',
   'https://amendablepredicamentvertical.com/p9m1gmpd?key=a3e88c6c6c485f3d3b80e2e54d76497a',
   'https://amendablepredicamentvertical.com/zew272d3wv?key=08a55baa42028feb47832b052f7f041d',
+  'https://amendablepredicamentvertical.com/ftevmc9ffu?key=88b2c5bdae56a3d0c808da01c98c12d7',
   'https://amendablepredicamentvertical.com/uhr5mmahw?key=e6341416b270ee9a62e0996299d61fa1',
-  'htps://amendablepredicamentvertical.com/nymnme7td?key=79b78598c8b159605c550bf724e60855'
+  'https://www.profitablegatetocontent.com/efem9wbsk?key=b2380e7e2441eb8676f2bfad8134ee97',
+  'https://www.profitablegatetocontent.com/i3c11jfyzq?key=1cd656c0804d50396e719a76a043e8e8',
+  'https://www.profitablegatetocontent.com/j1xw0x0rcx?key=1c9adcc6bca3296fac552e8c1e5b0a82',
+  'https://www.profitablegatetocontent.com/up6wxiqm91?key=f665e3bb7e8a4bf4c6c8cb38f693c707',
+  'https://www.profitablegatetocontent.com/jpp55717e?key=8507cef1a397afc5fea1dc360c0886b3'
+
   
   
   
