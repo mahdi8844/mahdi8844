51e3d3fd948753a4fb0fcb388aa6cf159db6cc1e
mahdi8844 <84512629+mahdi8844@users.noreply.github.com>
2022-10-20 10:44:22 +0800

    Update ansarinew2.js

--- ansarinew2.js
+++ ansarinew2.js
@@ -1,20 +1,20 @@
 var dataUrl = [
   
-  'https://www.highperformancegate.com/jpp55717e?key=8507cef1a397afc5fea1dc360c0886b3',
-  'https://www.highperformancegate.com/j1xw0x0rcx?key=1c9adcc6bca3296fac552e8c1e5b0a82',
-  'https://www.highperformancegate.com/efem9wbsk?key=b2380e7e2441eb8676f2bfad8134ee97',
-  'https://www.highperformancegate.com/i3c11jfyzq?key=1cd656c0804d50396e719a76a043e8e8',
-  'https://www.highperformancegate.com/up6wxiqm91?key=f665e3bb7e8a4bf4c6c8cb38f693c707',
+  'https://grantedpigsunborn.com/jg8vjg3xwy?key=edd04c0b0bd41c0bd8494c2890390ad4',
+  'https://grantedpigsunborn.com/eun6bc7z?key=7eb35efd04314c17bed2c3b7411852d9',
+  'https://grantedpigsunborn.com/feb7xbxrb?key=4e2587c12cf4b8990532d81ac580ce1f',
+  'https://grantedpigsunborn.com/mmj2k7nrdt?key=a9adec45b7e8413e8085337ca17f76bf',
+  'https://grantedpigsunborn.com/b9z3pexh7c?key=241a8547e9c0e7911d728d3ad6aac6b4',
   'https://wednesdaynaked.com/bq60261s?key=4268643d1293b7a87dda445d6f988ece',
   'https://wednesdaynaked.com/n6vefjuang?key=6d6c403700d7667dd3c9eb193df5c41d',
   'https://fireworkswad.com/nx1jfdzr?key=8a9b83430981254c00c2804ddb2b7d92',
-  'https://fireworkswad.com/yevb1vpg2?key=0127f56241a83ccd37506a3d59a797fe',
-  'https://fireworkswad.com/h8iv88se1v?key=21893a1ff00c4483973d3efc2febd04c',
+  'https://wednesdaynaked.com/n6vefjuang?key=6d6c403700d7667dd3c9eb193df5c41d',
+  'https://wednesdaynaked.com/bq60261s?key=4268643d1293b7a87dda445d6f988ece',
   'https://fireworkswad.com/kdvahsb0wj?key=66f1e59b1b551fb24893ae895dc1d658',
-  'https://themeillogical.com/j1rxj172?key=adc36aa55f17d2c41223d390f5d7be1e',
-  'https://themeillogical.com/xyhh9ebt?key=70bf1cf20c9c44d1a9b9079b1159855b',
-  'https://themeillogical.com/fx19rsb805?key=9a451e61fa6b94f030eb813c38aa046c',
-  'https://themeillogical.com/d33wd6ank?key=115c23b469ea61aa266586a912ebeac6',
+  'https://fireworkswad.com/h8iv88se1v?key=21893a1ff00c4483973d3efc2febd04c',
+  'https://fireworkswad.com/nx1jfdzr?key=8a9b83430981254c00c2804ddb2b7d92',
+  'https://fireworkswad.com/yevb1vpg2?key=0127f56241a83ccd37506a3d59a797fe',
+  'https://fireworkswad.com/rm5e0t7wd?key=d1c7d5a4bfef1f1a791c9f7c5cb38ec7',
   'https://themeillogical.com/xkd75qyzj?key=da41d9284ef2ccab8959f1b2e47c8fc7',
   'https://grantedpigsunborn.com/feb7xbxrb?key=4e2587c12cf4b8990532d81ac580ce1f',
   'https://grantedpigsunborn.com/b9z3pexh7c?key=241a8547e9c0e7911d728d3ad6aac6b4',
@@ -33,7 +33,6 @@ var dataUrl = [
   
   
   
-
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
